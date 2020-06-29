b0a13fb4ee4374958c0851357dca12556615e9a5
Alexsey <xomutoff.alex@gmail.com>
2020-06-29 10:52:16 +0300

    addTimer

--- js/script.js
+++ js/script.js
@@ -1,4 +1,7 @@
 window.addEventListener("DOMContentLoaded", () => {
+
+    //Tabs---------------------------
+
     let tabs = document.querySelectorAll(".tabheader__item"),
         tabsContent = document.querySelectorAll(".tabcontent"),
         tabsParent = document.querySelector(".tabheader__items");
@@ -37,4 +40,59 @@ window.addEventListener("DOMContentLoaded", () => {
             });
         }
     }); 
+
+    //Timer---------------------------
+
+    let deadline = "2020-07-01";
+
+        function getRemainingTime(endTime) {
+            let t = Date.parse(endTime) - Date.parse(new Date()),
+                days = Math.floor(t / (1000 * 60 * 60 * 24)),
+                hours = Math.floor((t / (1000 * 60 * 60) % 24)),
+                minutes = Math.floor((t / 1000  / 60) % 60),
+                seconds = Math.floor((t / 1000) % 60);
+            
+            return {
+                    "tottal": t,
+                    "days": days,
+                    "hours": hours,
+                    "minutes": minutes,
+                    "seconds": seconds
+                };
+        }
+        
+        function getZero(num) {
+            if (num >= 0 && num < 10) {
+                return `0${num}`;
+            } else {
+                return num;
+            }
+        }
+
+        function setClock (selector,endTime) {
+            let timer = document.querySelector(selector),
+                days = document.querySelector("#days"),
+                hours = document.querySelector("#hours"),
+                minutes = document.querySelector("#minutes"),
+                seconds = document.querySelector("#seconds"),
+                timeInterval = setInterval(updateClock, 1000);
+
+                updateClock(); 
+                
+            function updateClock() {
+                let t = getRemainingTime(endTime);
+
+                days.innerHTML = getZero(t.days);
+                hours.innerHTML = getZero(t.hours);
+                minutes.innerHTML = getZero(t.minutes);
+                seconds.innerHTML = getZero(t.seconds);
+
+                if(t.tottal <= 0){
+                    clearInterval(timeInterval);
+                }
+            }
+        }
+        setClock(".timer",deadline);
+        console.log(deadline);
+        
 });
