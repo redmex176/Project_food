72268afa69ee78396caeb137474c1fdf99342bb2
Alexsey <xomutoff.alex@gmail.com>
2020-07-16 10:51:55 +0300

    ChangedTimer

--- js/script.js
+++ js/script.js
@@ -1,38 +1,38 @@
-window.addEventListener("DOMContentLoaded", () => {
+"use stirct";
 
-    //Tabs---------------------------
+//Tabs
 
+document.addEventListener("DOMContentLoaded",()=> {
     let tabs = document.querySelectorAll(".tabheader__item"),
         tabsContent = document.querySelectorAll(".tabcontent"),
         tabsParent = document.querySelector(".tabheader__items");
 
-    function hideTabContent() {
-            tabsContent.forEach(item => {
-                item.classList.add("hide");
-                item.classList.remove("show", "fade");
-            });
+    function hideTabContent () {
+        tabsContent.forEach(item => {
+            item.classList.add("hide");
+            item.classList.remove("show");
+        });
 
-            tabs.forEach(item => {
-                item.classList.remove("tabheader__item_active");
-            });
-        }
-    
-    function showTabContent(i = 0){
-        
-        tabsContent[i].classList.add("show","fade");
-        tabsContent[i].classList.remove("hide");
-        
-        tabs[i].classList.add("tabheader__item_active");
-    } 
+        tabs.forEach(item => {
+            item.classList.remove("tabheader__item_active");
+        });
+    }
 
     hideTabContent();
+
+    function showTabContent(i = 0) {
+        tabsContent[i].classList.add("show");
+        tabsContent[i].classList.remove("hide");
+        tabs[i].classList.add("tabheader__item_active");
+    }
+
     showTabContent();
 
-   tabsParent.addEventListener("click", (event) => {
+    tabsParent.addEventListener("click", (event) => {
         let target = event.target;
-
-        if(target && target.classList.contains("tabheader__item")){
-            tabs.forEach((item, i) => {
+        
+        if(target && target.classList.contains("tabheader__item")) {
+            tabs.forEach((item,i) => {
                 if(target == item) {
                     hideTabContent();
                     showTabContent(i);
@@ -41,58 +41,60 @@ window.addEventListener("DOMContentLoaded", () => {
         }
     }); 
 
-    //Timer---------------------------
+//Timer
 
-    let deadline = "2020-07-01";
+    let deadline = "2020-07-25";
 
-        function getRemainingTime(endTime) {
-            let t = Date.parse(endTime) - Date.parse(new Date()),
-                days = Math.floor(t / (1000 * 60 * 60 * 24)),
-                hours = Math.floor((t / (1000 * 60 * 60) % 24)),
-                minutes = Math.floor((t / 1000  / 60) % 60),
-                seconds = Math.floor((t / 1000) % 60);
-            
+    function getRemainingTime (endTime) {
+        let t = Date.parse(endTime) - Date.parse(new Date()),
+            days = Math.floor((t / 86400000) % 24),
+            hours = Math.floor((t / 3600000) % 24),
+            minutes = Math.floor((t / 60000) % 60),
+            seconds = Math.floor((t / 1000) % 60);
+        
             return {
-                    "tottal": t,
-                    "days": days,
-                    "hours": hours,
-                    "minutes": minutes,
-                    "seconds": seconds
-                };
+                "total" : t,
+                "days" : days,
+                "hours" : hours,
+                "minutes" : minutes,
+                "seconds" : seconds
+            };
+    }
+
+    function getZero(num) {
+        if(num < 10) {
+            return `0${num}`;
+        } else {
+            return num;
         }
-        
-        function getZero(num) {
-            if (num >= 0 && num < 10) {
-                return `0${num}`;
-            } else {
-                return num;
+    }
+    function setClock (selector,endTime) {
+        let timer = document.querySelector(selector),
+            days = document.querySelector("#days"),
+            hours = document.querySelector("#hours"),
+            minutes = document.querySelector("#minutes"),
+            seconds = document.querySelector("#seconds"),
+            timeInterval = setInterval(updateClock,100);
+
+            
+            
+            updateClock();
+
+        function updateClock() {
+            let t = getRemainingTime(endTime);
+
+            days.innerHTML = getZero(t.days);
+            hours.innerHTML = getZero(t.hours);
+            minutes.innerHTML = getZero(t.minutes);
+            seconds.innerHTML = getZero(t.seconds);
+
+            if(t.total <= 0) {
+                clearInterval(timeInterval);
             }
+
         }
+    }
 
-        function setClock (selector,endTime) {
-            let timer = document.querySelector(selector),
-                days = document.querySelector("#days"),
-                hours = document.querySelector("#hours"),
-                minutes = document.querySelector("#minutes"),
-                seconds = document.querySelector("#seconds"),
-                timeInterval = setInterval(updateClock, 1000);
-
-                updateClock(); 
-                
-            function updateClock() {
-                let t = getRemainingTime(endTime);
-
-                days.innerHTML = getZero(t.days);
-                hours.innerHTML = getZero(t.hours);
-                minutes.innerHTML = getZero(t.minutes);
-                seconds.innerHTML = getZero(t.seconds);
-
-                if(t.tottal <= 0){
-                    clearInterval(timeInterval);
-                }
-            }
-        }
-        setClock(".timer",deadline);
-        console.log(deadline);
-        
+    setClock(".timer", deadline);
+    getRemainingTime(deadline);
 });
