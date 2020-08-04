fc3cb8efa2ca67c3bea4418b1ea410c5e993c8ac
Алексей <alex@MacBook-Pro-Aleksej.local>
2020-08-04 11:12:53 +0300

    addMenu

--- js/script.js
+++ js/script.js
@@ -43,7 +43,7 @@ window.addEventListener("DOMContentLoaded", ()=> {
         });
 
 //Timer
-        let deadline = "2020-07-25";
+        let deadline = "2020-08-10";
 
         function getRemainingTime(endTime) {
             let t = Date.parse(endTime) - Date.parse(new Date()),
@@ -83,6 +83,10 @@ window.addEventListener("DOMContentLoaded", ()=> {
             function updateClock(){
                 let t = getRemainingTime(endTime);
 
+                if (timeInterval <= 0) {
+                    clearInterval(timeInterval);
+                }
+
                 days.innerHTML = getZero(t.days);
                 hours.innerHTML = getZero(t.hours);
                 minutes.innerHTML = getZero(t.minutes);
@@ -97,14 +101,14 @@ window.addEventListener("DOMContentLoaded", ()=> {
 
 //Modal 
         let modal = document.querySelector(".modal"),
-            ModalCloseBtn = document.querySelector(".modal__close"),
-            modalTrigger = document.querySelectorAll("[data-modal]");
+            modalCloseBtn = document.querySelector(".modal__close"),
+            modalTrigger = document.querySelectorAll("[data-modal]"),
+            modalTimer = setTimeout(openModal,300000000);
 
-
-       
         function openModal() {
             modal.classList.add("show");
             document.body.style.overflow = "hidden";
+            clearInterval(modalTimer);   
         }
 
         function closeModal() {
@@ -116,7 +120,7 @@ window.addEventListener("DOMContentLoaded", ()=> {
             item.addEventListener("click", openModal);
         });
 
-        ModalCloseBtn.addEventListener("click",closeModal);
+        modalCloseBtn.addEventListener("click",closeModal);
 
         modal.addEventListener("click", (event) => {
             if (event.target == modal) {
@@ -130,6 +134,82 @@ window.addEventListener("DOMContentLoaded", ()=> {
             }
         });
        
-        setTimeout(openModal,30000);
+        function scrollModal () {
+            if (window.pageYOffset + document.documentElement.clientHeight == document.documentElement.scrollHeight){
+                openModal(); 
+                window.removeEventListener("scroll", scrollModal);
+            }
+        }
+        
+        window.addEventListener("scroll", scrollModal);
+
+        
+    //MenuCard 
+
+    class MenuCard {
+
+        constructor (src, alt, title, descr, price, parentSelector) { 
+            this.src = src;
+            this.alt = alt;
+            this.title = title;
+            this.descr = descr;
+            this.price = price;
+            this.cost = 70;
+            this.changeToRub();
+            this.parent = document.querySelector(parentSelector);
+        }
+
+        changeToRub() {
+           this.price = this.price * this.cost;
+        }
+
+        createMenu() {
+            let element = document.createElement("div");
+
+            element.innerHTML = `<div class="menu__item">
+            <img src=${this.src} alt=${this.alt}>
+            <h3 class="menu__item-subtitle">${this.title}</h3>
+            <div class="menu__item-descr">${this.descr}</div>
+            <div class="menu__item-divider"></div>
+            <div class="menu__item-price">
+                <div class="menu__item-cost">Цена:</div>
+                <div class="menu__item-total"><span>${this.price}</span> руб/день</div>
+            </div>
+        </div>`;
+
+        this.parent.append(element);
+        }
+        
+    }
+
+    new MenuCard(
+        "img/tabs/vegy.jpg",
+        "vegy",
+        'Меню "Фитнес"',
+        'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
+        3,
+        ".menu .container"
+
+    ).createMenu();
+
+    new MenuCard(
+        "img/tabs/elite.jpg",
+        "elite",
+        'Меню “Премиум”',
+        'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
+        5,
+        ".menu .container"
+
+    ).createMenu();
+
+    new MenuCard(
+        "img/tabs/post.jpg",
+        "post",
+        'Меню "Постное"',
+        'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.',
+        4,
+        ".menu .container"
+
+    ).createMenu();
 
 });
